925f880b1247424397b8ef1830bb2d7df37b24ac
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-05-16 16:33:40 +0600

    feat: integrate axios for data fetching in HomePage
    - Added axios as a dependency for improved data fetching capabilities.
    - Implemented getDataAxios function to fetch posts from an API using axios.
    - Retained existing fetch method for comparison and logged results for debugging.
    - Updated HomePage layout to display fetched post titles in a grid format.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const getDataFetch = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    // console.log("🚀 ~ getData ~ data:", data);
+    return data;
+  };
+
+  const getDataAxios = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      return res.data;
+    } catch (error) {
+      console.error("🚀 ~ getDataAxios ~ error:", error);
+    }
+  };
+
+  const data = await getDataAxios();
+  const dataFetch = await getDataFetch();
+  console.log("🚀 ~ HomePage ~ data:", data);
+  console.log("🚀 ~ HomePage ~ dataFetch:", dataFetch);
+
   return (
-    <main className="flex justify-center items-center h-screen w-full bg-slate-500">
-      <Image alt="" src="/vercel.svg" width={50} height={5} />
-      <Image alt="" src="/next.svg" width={100} height={100} />
-      {/* <Hero /> */}
+    <main className="flex flex-col justify-center items-center h-screen w-full bg-slate-500">
+      <section className="grid grid-cols-3 gap-2.5 place-items-center">
+        {data?.slice(0, 10).map((post: any) => (
+          <div
+            key={post.id}
+            className="border-2 border-white p-4 rounded-md w-full max-w-sm"
+          >
+            <h2>{post.title} </h2>
+            {/* <p>{post.body} </p> */}
+          </div>
+        ))}
+      </section>
     </main>
   );
 };
