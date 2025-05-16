fe619891db2ac91a3fbbc334afd4eabf22f46e30
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-05-16 14:57:00 +0600

    refactor: remove commented-out Button component from HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,6 @@ const HomePage = () => {
   return (
     <main>
       <Hero />
-      {/* <Button
-        title="Click me"
-        onClick={() => alert("Button clicked")}
-        phone={1234567890}
-      >
-        Click me
-      </Button> */}
     </main>
   );
 };
