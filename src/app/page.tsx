ef9b9916315091576553601af594fce8fe234138
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-05-16 15:07:27 +0600

    feat: update HomePage to include images and remove Hero component
    - Replaced Hero component with two Image components for visual enhancement.
    - Adjusted main layout styles for better centering and background color.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Hero } from "./_components/hero";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <main>
-      <Hero />
+    <main className="flex justify-center items-center h-screen w-full bg-amber-300/80">
+      <Image alt="" src="/vercel.svg" width={50} height={5} />
+      <Image alt="" src="/next.svg" width={100} height={100} />
+      {/* <Hero /> */}
     </main>
   );
 };
